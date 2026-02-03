7f009b183003fa4c9fa11dd0bb47f47734303f13
Batuhan Tunç <58087692+Batuhaanccs@users.noreply.github.com>
2026-02-04 01:42:50 +0300

    Time: 2380 ms (5.04%) | Memory: 19.4 MB (13.69%) - LeetSync

---
+++ 69-sqrtx/sqrtx.py
@@ -0,0 +1,9 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        i = 1
+        while True:
+            if i**2 == x:
+                return i
+            elif i**2 > x:
+                return i-1
+            i+=1
